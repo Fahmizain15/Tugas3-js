13e5f25cf3b2895c5d44c52375693df1523aaccf
rafiabdurrahman <merodrafi@gmail.com>
2022-02-02 22:02:33 -0800

    tugas3-js

---
+++ soal1.js
@@ -0,0 +1,13 @@
+function Lampu(warna){
+    if (warna == "Red"){
+        console.log("Stop");
+    }
+    else if (warna == "Yellow"){
+        console.log("Be Careful");
+    }
+    else if (warna == "Green"){
+        console.log("Go");
+    }
+    
+}
+Lampu("Red")
